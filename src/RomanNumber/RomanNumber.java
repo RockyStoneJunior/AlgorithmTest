47cc2a1cfe01e1c7e4c06eec69bf440f2fe96c2e
Administrator <511697856@qq.com>
2019-08-02 14:25:03 +0800

    Add the algorithm IntToRoman numbers

---
+++ src/RomanNumber/RomanNumber.java
@@ -0,0 +1,27 @@
+package RomanNumber;
+
+public class RomanNumber {
+
+    public static void main(String[] args){
+        System.out.println(intToRoman(3999));
+    }
+
+    public static String intToRoman(int num){
+        int [] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10 ,9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C" , "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int index = 0;
+
+        while (index < 13){
+            while(num >= nums[index]){
+                stringBuilder.append(romans[index] + " ");
+                num -= nums[index];
+            }
+
+            index++;
+        }
+
+        return stringBuilder.toString();
+    }
+}
