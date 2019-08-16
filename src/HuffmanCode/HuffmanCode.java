b329961cb165fa05845920b5e866dac9b19b18c0
Administrator <511697856@qq.com>
2019-08-16 15:42:38 +0800

    Add the algorithm encode and decode of HuffmanCode

--- src/HuffmanCode/HuffmanCode.java
+++ src/HuffmanCode/HuffmanCode.java
@@ -5,6 +5,7 @@ import PrintBinaryTree.TreeOperation;
 
 import java.lang.reflect.Array;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -54,7 +55,20 @@ public class HuffmanCode {
 
     public static void main(String[] args) {
         HuffmanCode huffmanCode = new HuffmanCode();
-        huffmanCode.huffmanCode(new char[]{'a', 'b', 'c', 'd', 'e', 'f' }, new int[]{5, 9, 12, 13, 16, 45});
+        HuffmanNode root = huffmanCode.huffmanCode(new char[]{'a', 'b', 'c', 'd', 'e', 'f' }, new int[]{5, 9, 12, 13, 16, 45});
+
+        huffmanCode.printCode(root, "");
+        TreeOperation.show(root);
+
+        //System.out.println(huffmanCode.huffmanDecode(root, "01001111000011111010101010101001010"));
+
+
+        Map<Character, String> codeMap = huffmanCode.getCodeMap(root, new HashMap<Character, String>(), "");
+        System.out.println(codeMap.toString());
+
+        String code = huffmanCode.getCode(codeMap, "abcdef");
+        System.out.println(code);
+        System.out.println(huffmanCode.huffmanDecode(root, code));
     }
 
 
@@ -69,7 +83,33 @@ public class HuffmanCode {
         printCode((HuffmanNode) root.right, s + "1");
     }
 
-    public Map<Character, String> huffmanCode(char[] charArr, int[] freqArr) {
+    public Map<Character, String> getCodeMap(HuffmanNode root, Map<Character, String> map, String s){
+
+        if (root.left == null && root.right == null) {
+
+            map.put(root.c, s);
+            return map;
+        }
+
+        getCodeMap((HuffmanNode) root.left, map, s + "0");
+        getCodeMap((HuffmanNode) root.right, map, s + "1");
+
+        return map;
+    }
+
+    public String getCode(Map<Character, String> codeMap, String s){
+
+        char[] charArr = s.toCharArray();
+        String result = new String();
+
+        for(char c : charArr){
+            result += codeMap.get(c);
+        }
+
+        return result;
+    }
+
+    public HuffmanNode huffmanCode(char[] charArr, int[] freqArr) {
 
         if(charArr.length != freqArr.length){
             return null;
@@ -87,11 +127,15 @@ public class HuffmanCode {
 
         while(q.size() > 1){
 
-            HuffmanNode x = q.peek();
-            q.poll();
+//            HuffmanNode x = q.peek();
+//            q.poll();
+//
+//            HuffmanNode y = q.peek();
+//            q.poll();
 
-            HuffmanNode y = q.peek();
-            q.poll();
+            HuffmanNode x = q.poll();
+
+            HuffmanNode y = q.poll();
 
             HuffmanNode f = new HuffmanNode('-', x.val + y.val, x, y);
 
@@ -99,10 +143,28 @@ public class HuffmanCode {
             q.add(f);
         }
 
-        printCode(root, "");
+        return root;
+    }
 
-        TreeOperation.show(root);
+    public String huffmanDecode(HuffmanNode root, String code){
 
-        return null;
+        int len = code.length();
+        HuffmanNode currNode = root;
+        StringBuffer result = new StringBuffer();
+
+        for(int i = 0; i < len; i++){
+            if(code.charAt(i) == '0'){
+                currNode = (HuffmanNode) currNode.left;
+            }else{
+                currNode = (HuffmanNode) currNode.right;
+            }
+
+            if(currNode.left == null && currNode.right == null){
+                result.append(currNode.c);
+                currNode = root;
+            }
+        }
+
+        return result.toString();
     }
 }
